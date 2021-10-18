2639cf7b8e54bab222c9535e3ebc1ecac6e3bc3a
Jack (Butch) Griffin <butchg@comcast.net>
2021-10-17 17:35:53 -0700

    small changes to enable simulator

--- src/main/java/frc/robot/PractBot.java
+++ src/main/java/frc/robot/PractBot.java
@@ -2,28 +2,42 @@ package frc.robot;
 
 import org.xero1425.base.XeroRobot;
 import org.xero1425.base.controllers.AutoController;
+import org.xero1425.misc.SimArgs;
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
  * project.
  */
 public class PractBot extends XeroRobot {
   public PractBot() {
-    super(0.02) ;
+    super(0.02);
   }
-  
+
   public String getName() {
     return "PractBot";
   }
 
   public AutoController createAutoController() {
-    return null ;
+    return null;
   }
-  
+
   protected void hardwareInit() throws Exception {
   }
 
-  //support simualation
+  public String getSimulationFileName() {
+    String ret = SimArgs.InputFileName;
+    if (ret != null)
+      return ret;
+
+    return "nop";
+  }
+
+  protected void addRobotSimulationModels() {
+    // ModelFactory factory = SimulationEngine.getInstance().getModelFactory();
+    // factory.registerModel("turret", "TBD");
+    // factory.registerModel("arm", "TBD");
+  }
 }
