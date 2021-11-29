085076c5dfac26b738e58ccb8154dfdcd68316ab
aditi-bhaskar <aditi.bhaskar@gmail.com>
2021-11-29 01:42:02 +0000

    added new gate system to conveyor subsystem - left and right gaes + actions

--- src/main/java/frc/robot/automodes/TwoToteAuto.java
+++ src/main/java/frc/robot/automodes/TwoToteAuto.java
@@ -6,14 +6,15 @@ import org.xero1425.base.motorsubsystem.MotorPowerAction;
 import org.xero1425.base.tankdrive.TankDrivePathFollowerAction;
 import org.xero1425.base.tankdrive.TankDriveSubsystem;
 
+import frc.robot.conveyor.ConveyorDeployRightAction;
+import frc.robot.conveyor.ConveyorOffAction;
 import frc.robot.conveyor.ConveyorSubsystem;
 import frc.robot.intake.IntakeSubsystem;
 
 public class TwoToteAuto extends BunnyBotAutoMode {
 
-    private Action conveyor_deposit_action_ ;
-    private Action conveyor_stop_action_ ; 
-
+    private Action conveyor_deploy_right_action_;
+    private Action conveyor_off_action_ ; 
 
     public TwoToteAuto(BunnyBotAutoController ctrl, String name, String path1, String path2, String delay) 
             throws Exception {
@@ -24,22 +25,24 @@ public class TwoToteAuto extends BunnyBotAutoMode {
         IntakeSubsystem intake = getBunnyBotSubsystem().getIntake() ;
         MotorPowerAction intake_on_act_ = new MotorPowerAction(intake, "motor:on:power") ;
 
-        conveyor_deposit_action_ = new MotorPowerAction(conveyor, "motor:on:power") ;
-        conveyor_stop_action_ = new MotorPowerAction(conveyor, 0.0) ;
+        //need a seperate automode if we're doing Right vs Left deploying
+        conveyor_deploy_right_action_ = new ConveyorDeployRightAction(conveyor, true);
+        conveyor_off_action_ = new ConveyorOffAction(conveyor, true);
 
         //turn on intake
         addSubActionPair(intake, intake_on_act_, false) ;
         //drive 1st path
         addSubActionPair(db, new TankDrivePathFollowerAction(db, path1, false), true) ;
         //deposit in 1st tote
-        addSubActionPair(conveyor, conveyor_deposit_action_, false) ;
+        addSubActionPair(conveyor, conveyor_deploy_right_action_, false);
         addAction(new DelayAction(getAutoController().getRobot(), delay)) ;
-        addSubActionPair(conveyor, conveyor_stop_action_, false) ;
+        addSubActionPair(conveyor, conveyor_off_action_, false) ;
         //drive 2nd path
         addSubActionPair(db, new TankDrivePathFollowerAction(db, path2, false), true) ;
         //deposit in 2nd tote
-        addSubActionPair(conveyor, conveyor_deposit_action_, false) ;
+        addSubActionPair(conveyor, conveyor_deploy_right_action_, false);
         addAction(new DelayAction(getAutoController().getRobot(), delay)) ;
-        addSubActionPair(conveyor, conveyor_stop_action_, false) ;
+        addSubActionPair(conveyor, conveyor_off_action_, false) ;
+        
     }
 }
---
+++ src/main/java/frc/robot/conveyor/ConveyorOffAction.java
@@ -0,0 +1,69 @@
+package frc.robot.conveyor;
+
+import org.xero1425.base.motors.BadMotorRequestException;
+import org.xero1425.base.motorsubsystem.MotorPowerAction;
+import org.xero1425.misc.BadParameterTypeException;
+import org.xero1425.misc.MissingParameterException;
+
+import frc.robot.conveyor.ConveyorSubsystem.LeftGatePosition;
+import frc.robot.conveyor.ConveyorSubsystem.RightGatePosition;
+
+public class ConveyorOffAction extends MotorPowerAction {
+
+    private ConveyorSubsystem sub_ ;
+    private ConveyorSubsystem.LeftGatePosition l_pos_ ;
+    private ConveyorSubsystem.RightGatePosition r_pos_ ;
+    private boolean setready_ ;
+
+    public ConveyorOffAction(ConveyorSubsystem conveyor, boolean setready)
+            throws BadParameterTypeException, MissingParameterException, BadMotorRequestException {
+        super(conveyor, 0.0) ;
+        
+        sub_ = conveyor ;
+        setready_ = setready ;
+    }
+
+    @Override
+    public void start() throws Exception {
+        super.start() ;
+
+    }
+
+    @Override
+    public void run() {
+        super.run() ;
+        sub_.setLeftGate(LeftGatePosition.CLOSED);
+        sub_.setRightGate(RightGatePosition.CLOSED);
+
+    }
+
+    @Override
+    public void cancel() {
+        super.cancel() ;
+    }
+
+    @Override 
+    public String toString(int indent) {
+        String ret = prefix(indent) + "ConveyorOffAction" ;
+        
+        ret += " Left Gate is = " ;
+        if (l_pos_ == ConveyorSubsystem.LeftGatePosition.OPEN)
+            ret += " Open" ;
+        else
+            ret += " Closed" ;
+        
+        ret += " Right Gate is = " ;
+        if (r_pos_ == ConveyorSubsystem.RightGatePosition.OPEN)
+            ret += " Open" ;
+        else
+            ret += " Closed" ;
+
+        if (setready_)
+            ret += " true" ;
+        else
+            ret += " false" ;
+            
+        return ret ;
+    }
+
+}
