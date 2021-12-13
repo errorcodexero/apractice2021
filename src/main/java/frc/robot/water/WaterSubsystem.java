681addf08831e2b7f76cc247f65aed68523a40ac
aditi-bhaskar <aditi.bhaskar@gmail.com>
2021-12-13 15:29:24 +0000

    water subsystem is now a motorsubsystem

--- src/main/java/frc/robot/water/WaterSubsystem.java
+++ src/main/java/frc/robot/water/WaterSubsystem.java
@@ -1,64 +1,26 @@
 package frc.robot.water;
 
 import org.xero1425.base.Subsystem;
+import org.xero1425.base.motorsubsystem.MotorPowerAction;
+import org.xero1425.base.motorsubsystem.MotorSubsystem;
 
-import edu.wpi.first.wpilibj.Relay;
+// TODO: add water subsystem into OI device + 2tote automode
 
-// TODO: CHANGE WATER TO MOTOR POWER SUBSYSTEM
-
-public class WaterSubsystem {
+public class WaterSubsystem extends MotorSubsystem {
     public static final String SubsystemName = "water";
 
-    private Relay water_relay_ ;
-    // TODO : add the hardware stuff for the up/down water shooters - might be a relay, may not be one tho...
-
-    enum OnOffState {
-        ON,
-        OFF, 
-        UNKNOWN
+    public WaterSubsystem(Subsystem parent) {
+        super(parent, SubsystemName) ;
     }
 
-    private OnOffState water_relay_state_ = OnOffState.UNKNOWN;
-    private OnOffState squirt_up_state_ = OnOffState.UNKNOWN;
-    private OnOffState squirt_down_state_ = OnOffState.UNKNOWN;
-
-    public WaterSubsystem(Subsystem parent) throws Exception {
-        water_relay_ = new Relay(0, Relay.Direction.kForward) ; 
-        // TODO: get the "channel" for relay (currently set to "0") from params file
-
+    @Override
+    public void postHWInit() {
+        setDefaultAction(new MotorPowerAction(this, 0.0));
     }
 
-    public void setPump (OnOffState relay) {
-        if (relay == OnOffState.ON) {
-            water_relay_state_ = OnOffState.ON ;
-            water_relay_.set(Relay.Value.kOn) ;
-        }
-        else if (relay == OnOffState.OFF) {
-            water_relay_state_ = OnOffState.OFF ;
-            water_relay_.set(Relay.Value.kOff) ;
-        }
+    public void setWaterPower(double p) {
+        setPower(p) ;
     }
-
-
-    // TODO: set the hardware for all 4 following cases of setting squirter state
-    public void setSquirtUp (OnOffState squirter) {
-        if (squirter == OnOffState.ON) {
-            squirt_up_state_ = OnOffState.ON ;
-        }
-        else if (squirter == OnOffState.ON) {
-            squirt_up_state_ = OnOffState.OFF ;
-        }
-    }
-    
-    public void setSquirtDown (OnOffState squirter) {
-        if (squirter == OnOffState.ON) {
-            squirt_down_state_ = OnOffState.ON ;
-        }
-        else if (squirter == OnOffState.ON) {
-            squirt_down_state_ = OnOffState.OFF ;
-        }
-    }
-
 
 }
 
